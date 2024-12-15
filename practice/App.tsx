57abea1d219c470f55681ab12f4f3c8e725a5f01
srikanth <nag1024@srikanths-MacBook-Pro.local>
2024-12-15 17:53:16 +0530

    deeplink for android its working fine

--- practice/App.tsx
+++ practice/App.tsx
@@ -5,14 +5,45 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SplashScreen from "./component/SplashScreen";
 import OnBordingScreen from "./component/OnBordingScreen";
 const Stack = createNativeStackNavigator();
-const SplashscreenStack=( props:any)=>(
-  <Stack.Navigator  initialRouteName="SplashScreen" screenOptions={{headerShown:false ,}} >
+
+function Testaa(){
+  return<View><Text>hh</Text></View>
+}
+function App() {
+  const linking = {
+    
+       prefixes: ["https://example"],  
+     
+       config: {
+        
+         screens: {
+          OnBordingScreen: {     // ====> Screen name to navigate
+             path: "demo/:id",   // ===> path to which given url should match
+             parse: {
+               id: (id: any) => `${id}`,
+             },
+           },
+           SplashScreen: {
+             path: "changepswd/token/:id",  //===> :id is param that we pass
+             parse: {
+               id: (id: any) => `${id}`,
+             },
+           },
+           Testaa: {
+            path: "Testaa",  //===> :id is param that we pass
+            parse: {
+              id: (id: any) => `${id}`,
+            },
+          },
+         },
+       },
+     };
+    
+  return (<NavigationContainer linking={linking} ><Stack.Navigator  initialRouteName="SplashScreen" screenOptions={{headerShown:false ,}} >
     <Stack.Screen  name="SplashScreen"  component={SplashScreen} />
     <Stack.Screen name="OnBordingScreen"  component={OnBordingScreen} />
-  </Stack.Navigator>
-)
-function App() {
-  return (<NavigationContainer><SplashscreenStack/></NavigationContainer>)
+    <Stack.Screen name="Testaa"  component={Testaa} />
+  </Stack.Navigator></NavigationContainer>)
 }
 
 export default App
